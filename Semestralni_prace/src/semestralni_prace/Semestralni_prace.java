cef712de602b853bef40b089a1db84d99ec5ee2b
Petr <Petr@147.32.218.194>
2013-02-14 14:48:43 +0100

    pokusnej commit

---
+++ Semestralni_prace/src/semestralni_prace/Semestralni_prace.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package semestralni_prace;
+
+/**
+ *
+ * @author Petr
+ */
+public class Semestralni_prace {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
