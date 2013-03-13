7ef9136292e765222f9b162e0768f1d831626cf1
petr <petr@petr-notebook>
2013-03-13 17:39:12 +0100

    Zadání semestrální práce

--- Semestralni_prace/src/semestralni_prace/Semestralni_prace.java
+++ Semestralni_prace/src/semestralni_prace/Semestralni_prace.java
@@ -13,6 +13,6 @@ public class Semestralni_prace {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Grafická kalkulačka");
     }
 }
